a8b4bc7b58980cdc1e16b87a6c709ccae7b5882c
hamza <darejhamza@gmail.com>
2021-03-24 15:30:33 +0100

    portFolio

---
+++ script/index.js
@@ -0,0 +1,10 @@
+let clicked = true;
+document.querySelector("#menuBox").addEventListener("click", () => {
+  clicked = !clicked;
+  console.log(clicked);
+  if (clicked) {
+    document.querySelector(".rightNav").style.cssText = "display:flex;";
+  } else {
+    document.querySelector(".rightNav").style.cssText = "display:none ;";
+  }
+});
